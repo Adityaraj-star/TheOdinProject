87a7ba225ad424ec972a080ac96dd4a0e14b481e
Aditya Raj <adityaraj93107@gmail.com>
2025-07-10 00:06:12 +0530

    added some raw data

---
+++ scripts/data/books.js
@@ -0,0 +1,17 @@
+const books = [
+    {
+       title: 'The Alchemist',
+       author: 'Paulo Coelho',
+       pages: 208 
+    },
+    {
+       title: '1984',
+       author: 'George Orwell',
+       pages: 328
+    },
+    {
+       title: 'To Kill a Mockingbird',
+       author: 'Harper Lee',
+       pages: 336 
+    }
+]
