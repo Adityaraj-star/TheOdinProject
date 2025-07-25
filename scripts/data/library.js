c79bdd97854c3f03c3364757d59079e511e040ae
Aditya Raj <adityaraj93107@gmail.com>
2025-07-25 13:17:09 +0530

    changed name of file and array

---
+++ scripts/data/library.js
@@ -0,0 +1,73 @@
+export const library = [
+   {
+      id: 1,
+      title: 'The Alchemist',
+      author: 'Paulo Coelho',
+      pages: 208,
+      status: 'unread'
+   },
+   {
+      id: 2,
+      title: '1984',
+      author: 'George Orwell',
+      pages: 328,
+      status: 'unread'
+   },
+   {
+      id: 3,
+      title: 'To Kill a Mockingbird',
+      author: 'Harper Lee',
+      pages: 336,
+      status: 'unread'
+   },
+   {
+      id: 4,
+      title: 'Pride and Prejudice',
+      author: 'Jane Austen',
+      pages: 279,
+      status: 'unread'
+  },
+  {
+      id: 5,
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      pages: 180,
+      status: 'unread'
+  },
+  {
+      id: 6,
+      title: 'Moby-Dick',
+      author: 'Herman Melville',
+      pages: 635,
+      status: 'unread'
+  },
+  {
+      id: 7,
+      title: 'The Catcher in the Rye',
+      author: 'J.D. Salinger',
+      pages: 277,
+      status: 'unread'
+  },
+  {
+      id: 8,
+      title: 'The Hobbit',
+      author: 'J.R.R. Tolkien',
+      pages: 310,
+      status: 'unread'
+  },
+  {
+      id: 9,
+      title: 'Fahrenheit 451',
+      author: 'Ray Bradbury',
+      pages: 194,
+      status: 'unread'
+  },
+  {
+      id: 10,
+      title: 'Brave New World',
+      author: 'Aldous Huxley',
+      pages: 268,
+      status: 'unread'
+  }
+
+]
