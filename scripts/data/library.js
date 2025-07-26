355f5e5aec527325bebb8b746d9d4a861c8ae97b
Aditya Raj <adityaraj93107@gmail.com>
2025-07-26 14:10:07 +0530

    changed id

--- scripts/data/library.js
+++ scripts/data/library.js
@@ -1,69 +1,69 @@
 export const library = [
    {
-      id: 1,
+      id: crypto.randomUUID(),
       title: 'The Alchemist',
       author: 'Paulo Coelho',
       pages: 208,
       status: 'unread'
    },
    {
-      id: 2,
+      id: crypto.randomUUID(),
       title: '1984',
       author: 'George Orwell',
       pages: 328,
       status: 'unread'
    },
    {
-      id: 3,
+      id: crypto.randomUUID(),
       title: 'To Kill a Mockingbird',
       author: 'Harper Lee',
       pages: 336,
       status: 'unread'
    },
    {
-      id: 4,
+      id: crypto.randomUUID(),
       title: 'Pride and Prejudice',
       author: 'Jane Austen',
       pages: 279,
       status: 'unread'
   },
   {
-      id: 5,
+      id: crypto.randomUUID(),
       title: 'The Great Gatsby',
       author: 'F. Scott Fitzgerald',
       pages: 180,
       status: 'unread'
   },
   {
-      id: 6,
+      id: crypto.randomUUID(),
       title: 'Moby-Dick',
       author: 'Herman Melville',
       pages: 635,
       status: 'unread'
   },
   {
-      id: 7,
+      id: crypto.randomUUID(),
       title: 'The Catcher in the Rye',
       author: 'J.D. Salinger',
       pages: 277,
       status: 'unread'
   },
   {
-      id: 8,
+      id: crypto.randomUUID(),
       title: 'The Hobbit',
       author: 'J.R.R. Tolkien',
       pages: 310,
       status: 'unread'
   },
   {
-      id: 9,
+      id: crypto.randomUUID(),
       title: 'Fahrenheit 451',
       author: 'Ray Bradbury',
       pages: 194,
       status: 'unread'
   },
   {
-      id: 10,
+      id: crypto.randomUUID(),
       title: 'Brave New World',
       author: 'Aldous Huxley',
       pages: 268,
