b012567a73f75d559a7f94dc03b484d31834a4b1
Aditya Raj <adityaraj93107@gmail.com>
2025-07-26 14:05:58 +0530

    Added book form dialog functionality and retriece form input on confirm

--- scripts/readify.js
+++ scripts/readify.js
@@ -1,21 +1,29 @@
 import { library } from "./data/library.js";
 
-function Book(title, author, pages, status) {
-    this.id = crypto.randomUUID();
+const addBookBtn = document.querySelector(".js-add-book-btn");
+const formDialog = document.querySelector(".js-form-dialog");
+const bookForm = document.querySelector(".js-book-form");
+const bookTitle = document.querySelector(".js-book-title");
+const bookAuthor = document.querySelector(".js-book-author");
+const bookPages = document.querySelector(".js-book-pages");
+const bookStatus = document.querySelector(".js-book-status");
+const confirmBtn = document.querySelector(".js-confirm-btn");
+
+
+function Book(id, title, author, pages, status) {
+    this.id = id
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
 }
 
-function addBookToLibrary(title, author, pages, status) {
-    const book = new Book(title, author, pages, status);
+function addBookToLibrary(bookId, bookTitle, bookAuthor, bookPages, bookStatus) {
+    const book = new Book(bookId, bookTitle, bookAuthor, bookPages, bookStatus);
     
     library.push(book);
     renderBooks();
 }
-
-addBookToLibrary("Data Science", "JK", 500, "read");
 
 function renderBooks() {
     let booksHTML = '';
@@ -41,5 +49,41 @@ function renderBooks() {
 
 renderBooks();
 
-console.log(library);
+
+addBookBtn.addEventListener('click', () => {
+    formDialog.showModal();
+});
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+confirmBtn.addEventListener('click', () => {
+    formDialog.close();
+})
+
+
+function retrieveBookData() {
+    let title, author, pages, status;
+    confirmBtn.addEventListener('click', () => {
+        title = bookTitle.value;
+        author = bookAuthor.value;
+        pages = bookPages.value;
+
+        // checked property tells if checkbox is checked or not
+        if (bookStatus.checked) {
+            status = "read";
+        } else {
+            status = "unread";
+        }
+
+        addBookToLibrary(crypto.randomUUID(), title, author, pages, status);
+    });
+
+
+}
+
+retrieveBookData();
+
+
 
