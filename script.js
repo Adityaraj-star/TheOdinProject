147001f06749cd9c74233701a9d2af457c2323cd
Aditya Raj <adityaraj93107@gmail.com>
2025-09-02 12:16:47 +0530

    created GameBoard module

---
+++ script.js
@@ -0,0 +1,24 @@
+const GameBoard = (function () {
+    let board = ['', '', '', '', '', '', '', '', '']; //Private Variable
+
+    //Public Functions
+    function getBoard() {
+        return board;
+    }
+
+    function placeMark(index, marker) {
+        board[index] = marker;
+    }
+
+    function resetBoard() {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = '';
+        }
+     }
+
+    return {
+        getBoard,
+        placeMark,
+        resetBoard
+    }
+})();
