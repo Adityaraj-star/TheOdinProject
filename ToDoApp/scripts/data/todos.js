9d005fda8911202ee2b27c436cb4d22a856e39f2
Aditya Raj <adityaraj93107@gmail.com>
2025-10-22 15:00:31 +0530

    Add initial seed data for tasks

---
+++ ToDoApp/scripts/data/todos.js
@@ -0,0 +1,21 @@
+export const todos = [
+    {
+        id: 1,
+        title: "Study Data Structures and Algorithms",
+        description: "Continue through the textbook until page 130.",
+        dueDate: "2025-10-21",
+        priority: "Important",
+        projectSection: "# DSA",
+        isCompleted: false
+    },
+
+    {
+        id: 2,
+        title: "Submit Project Proposal",
+        description: "Finalize the proposal document and email it to the professor.",
+        dueDate: "2025-10-24",
+        priority: "urgent",
+        projectSection: "# University",
+        isCompleted: false
+    }
+]
