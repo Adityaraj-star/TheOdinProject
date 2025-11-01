028219312b565ec6f3d40fa46f9d1eb2f705cf62
Aditya Raj <adityaraj93107@gmail.com>
2025-11-02 00:44:22 +0530

    Implement 'Add Project' functionality'

--- ToDoApp/scripts/dom.js
+++ ToDoApp/scripts/dom.js
@@ -1,5 +1,6 @@
-import { addTasksToTodos } from "./data/todos.js";
-import { rendertaskCards } from "./main.js";
+import { addTodo } from "./data/todos.js";
+import { addProjects } from "./data/projects.js";
+import { renderTodoCards, renderProjectCards } from "./render.js";
 
 const formDialog = document.querySelector(".js-form-dialog");
 const addTodoIcon = document.querySelector(".js-add-icon");
@@ -12,8 +13,15 @@ const todoDueDateBox = document.querySelector(".js-todo-dueDate");
 const todoPriorityBox = document.querySelector(".js-todo-priority");
 const todoInboxBox = document.querySelector(".js-todo-inbox");
 
-export function setupEventListeners() {
+const projectDialog = document.querySelector(".js-project-dialog");
+const addProjectIcon = document.querySelector(".js-add-project-icon");
+const cancelProjectBtn = document.querySelector(".js-cancel-project-btn");
+const addProjectForm = document.querySelector(".js-addProject-form");
 
+const projectNameBox = document.querySelector(".js-project-name");
+
+
+export function setupEventListeners() {
     addTodoIcon.addEventListener('click', () => {
         formDialog.showModal();
     });
@@ -36,14 +44,35 @@ export function setupEventListeners() {
             return;
         }
 
-        addTasksToTodos(todoName, todoDescription, todoDueDate, todoPriority, todoInbox);
-
-        rendertaskCards();
-
+        addTodo(todoName, todoDescription, todoDueDate, todoPriority, todoInbox);
+        renderTodoCards();
         formDialog.close();
     });
 
     formDialog.addEventListener('close', () => {
         addToDoForm.reset();
+    });
+
+    addProjectIcon.addEventListener('click', () => {
+        projectDialog.showModal();
+    });
+
+    cancelProjectBtn.addEventListener('click', () => {
+        projectDialog.close();
+    });
+
+    addProjectForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const projectName = projectNameBox.value;
+
+        if (projectName.trim() === '') {
+            alert('Please enter a project name.');
+            return;
+        }
+
+        addProjects(projectName);
+        renderProjectCards();
+        projectDialog.close();
     });
 }
